e5f283fb791569521206d73a1ac11c40cb951615
jhchein <hehein@microsoft.com>
2023-05-02 17:49:44 +0200

    changed the function to allow anonymous access

--- function_app.py
+++ function_app.py
@@ -39,7 +39,7 @@ def detect_anomalies(
 
 
 @app.function_name(name="AnomalyDetection")
-@app.route(route="anomalydetection")
+@app.route(route="anomalydetection", auth_level=func.AuthLevel.ANONYMOUS)
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
 
