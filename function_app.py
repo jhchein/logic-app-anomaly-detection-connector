efa4b83a275b5e6825a5f847c9e577e46b5b7f59
jhchein <hehein@microsoft.com>
2023-05-03 10:47:24 +0200

    Added function level authentication

--- function_app.py
+++ function_app.py
@@ -39,7 +39,7 @@ def detect_anomalies(
 
 
 @app.function_name(name="AnomalyDetection")
-@app.route(route="anomalydetection", auth_level=func.AuthLevel.ANONYMOUS)
+@app.route(route="anomalydetection", auth_level=func.AuthLevel.FUNCTION)
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
 
